4ed67b5fc2f6a6e7a60fd7f05e99bd962f01448e
Spencer Ley <spencerley234@gmail.com>
2023-10-25 12:25:09 +0100

    tables created and seed data for weeks, workshops and bookmarks complete

--- db/scripts/reset-database.js
+++ db/scripts/reset-database.js
@@ -4,49 +4,80 @@ async function resetDatabase() {
   try {
     // Drop existing tables if they exist
     await pool.query(`
-      DROP TABLE IF EXISTS errors CASCADE;
-      DROP TABLE IF EXISTS responses CASCADE;
+      DROP TABLE IF EXISTS workshops CASCADE;
+      DROP TABLE IF EXISTS weeks CASCADE
+      DROP TABLE IF EXISTS bookmarks CASCADE;
+      DROP TABLE IF EXISTS comments CASCADE;
     `);
 
-    // Create the errors table
+    // Create the weeks table
     await pool.query(`
-      CREATE TABLE errors (
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        description TEXT NOT NULL,
-        workshop VARCHAR(255),
-        error_code VARCHAR(255),
-        error_message TEXT
-      );
-    `);
-
-    // Create the responses table with a foreign key to the authors table
-    await pool.query(`
-        CREATE TABLE responses (
+        CREATE TABLE weeks (
           id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-          cause TEXT NOT NULL,
-          solution TEXT NOT NULL,
-          error_id INT REFERENCES errors(id) ON DELETE CASCADE NOT NULL
+          description TEXT NOT NULL,
         );
     `);
 
-    // Seed the errors table
+    // Create the workshops table with foreign key to weeks table
     await pool.query(`
-      INSERT INTO errors (description, workshop, error_code, error_message)
-      VALUES
-      ('Network Error', 'Workshop A', 'ERR-001', 'Failed to connect to the server.'),
-      ('File Not Found', 'Workshop B', 'ERR-002', 'The requested file does not exist.'),
-      ('Database Connection Error', 'Workshop C', 'ERR-003', 'Unable to establish a database connection.'),
-      ('Permission Denied', 'Workshop A', 'ERR-004', 'You do not have permission to access this resource.');
+      CREATE TABLE workshops (
+        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name TEXT NOT NULL,
+        description TEXT,
+        date DATE,
+        week_id INT REFERENCES weeks(id) ON DELETE CASCADE NOT NULL
+      );
     `);
 
-    // Seed the books table
+    // Create the bookmarks table with a foreign key to the workshops table
     await pool.query(`
-        INSERT INTO responses (cause, solution, error_id)
+        CREATE TABLE bookmarks (
+          id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+          description TEXT NOT NULL,
+          url TEXT NOT NULL,
+          helpful_links BOOLEAN,
+          workshop_id INT REFERENCES workshops(id) ON DELETE CASCADE NOT NULL
+        );
+    `);
+
+    // Create the comments table with a foreign key to the workshops table
+    await pool.query(`
+        CREATE TABLE comments (
+          id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+          comment TEXT NOT NULL,
+          added_date DATE,
+          workshop_id INT REFERENCES workshops(id) ON DELETE CASCADE NOT NULL
+        );
+    `);
+
+    // Seed the weeks table
+    await pool.query(`
+      INSERT INTO weeks (description)
+      VALUES
+      ('Week 1 - Computational Thinking'),
+      ('Week 2 - JavaScript Foundations'),
+      ('Week 3 - Front End Foundations');
+    `);
+
+    // Seed the workshops table
+    await pool.query(`
+        INSERT INTO workshops (name, description, date, week_id)
         VALUES
-        ('Invalid input data', 'Please check the data format and try again.', 1),
-        ('Network timeout', 'Ensure your internet connection is stable and try again.', 2),
-        ('Missing authentication token', 'Log in to your account to access this feature.', 3),
-        ('Database server down', 'Contact the system administrator to resolve the issue.', 4);
+        ('Workshop 1 - Romeo and Juligit', 'Description Placeholder', '2023-09-07', 1),
+        ('Hackathon - Escape Room on Scratch', 'Description Placeholder', '2023-09-08', 1),
+        ('Workshop 1 - Intro to JavaScript', 'Description Placeholder', '2023-09-11', 2),
+        ('Workshop 2 - Loops', 'Description Placeholder', '2023-09-12', 2),
+        ('Workshop 1 - Debugging', 'Description Placeholder', '2023-09-18', 3),
+        ('Workshop 2 - Intro to the DOM', 'Description Placeholder', '2023-09-19', 3);
+    `);
+
+    // Seed the bookmarks table
+    await pool.query(`
+        INSERT INTO bookmarks (description, url, helpful_links, workshop_id)
+        VALUES
+        ('Github Repo', 'https://classroom.github.com/a/ElP3Fc9C', FALSE, 1),
+        ('Hackathon Repo', 'https://classroom.github.com/assignment-invitations/3fbdc8e4f8e580a676471e598d77ea92/status', FALSE, 2),
+        ('Scratch Link', 'https://scratch.mit.edu/', TRUE, 2);
     `);
 
     console.log("Database reset successful");
